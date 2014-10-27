1181558f38479b8fad1386cf44919b05775cb236
satanicsanta <elifosterwy@gmail.com>
2014-10-26 20:08:35 -0700

    0.2

--- src/navitem.py
+++ src/navitem.py
@@ -1,5 +1,5 @@
 __author__ = 'elijahfoster-wysocki'
-#Version 0.1
+#Version 0.2
 
 import shutil
 import tempfile
@@ -8,18 +8,21 @@ import tempfile
 directory = 'work/input.txt'
 
 #These can be found on Template:G/Mods.
-modname = raw_input("What is the mod abbreviation?\n")
+modname = raw_input("What is the mod abbreviation?\nIf you are unsure, or it is not on the list, please contact Santa. If you are Santa, do it.\n")
 
 tmp = tempfile.NamedTemporaryFile(delete=False)
 with open(directory) as finput:
     with open(tmp.name, 'w') as ftmp:
         for line in finput:
             x = str(line)
-            ftmp.write('{{NI|' + x.rstrip("\r\n") + "|mod=" + modname + "}}{{,}}\n")
+            ftmp.write('    -->{{NI|' + x.rstrip("\r\n") + "|mod=" + modname + "}}{{,}}<!--\n")
 shutil.move(tmp.name, directory)
 
 '''
 == Changelog ==
+=== 0.2 ===
+* fixed the text that is written.
+* changed modname raw_input text
 === 0.1 ===
 * initial
 '''
