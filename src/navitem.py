5c61716e8574e3b7c3d498a1ffffb940eda5a638
satanicsanta <elifosterwy@gmail.com>
2014-10-26 20:50:21 -0700

    hopefully fix shit

--- src/navitem.py
+++ src/navitem.py
@@ -3,11 +3,15 @@ __author__ = 'elijahfoster-wysocki'
 
 import shutil
 import tempfile
+import sys
+import os
 
 # EDIT THIS IF YOU HAVE CHANGED YOUR DIRECTORY NAMES.
 directory = 'work/input.txt'
+new_dir = 'work/udungoofd.txt'
 
 #These can be found on Template:G/Mods.
+#If you are running Python 3, change raw_input to input for it to work. You may still get errors though.
 modname = raw_input("What is the mod abbreviation?\nIf you are unsure, or it is not on the list, please contact Santa. If you are Santa, do it.\n")
 
 tmp = tempfile.NamedTemporaryFile(delete=False)
@@ -16,10 +20,19 @@ with open(directory) as finput:
         for line in finput:
             x = str(line)
             ftmp.write('    -->{{NI|' + x.rstrip("\r\n") + "|mod=" + modname + "}}{{,}}<!--\n")
-shutil.move(tmp.name, directory)
+try:
+    shutil.move(tmp.name, directory)
+except:
+    print "Permission Error/IOError! Trying something else save the file!"
+    os.rename(directory, new_dir)
+
+sys.exit()
 
 '''
 == Changelog ==
+=== 0.3 ===
+* hopefully fixed the Windows issue
+* it now exits properly using sys.exit()
 === 0.2 ===
 * fixed the text that is written.
 * changed modname raw_input text
