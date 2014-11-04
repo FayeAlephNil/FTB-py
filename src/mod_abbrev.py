bc121460f24d0320a343533c99c4e9c974ba819b
satanicsanta <elifosterwy@gmail.com>
2014-11-03 20:23:21 -0800

    got rid of old gui program, got working on new per program stuff

--- src/mod_abbrev.py
+++ src/mod_abbrev.py
@@ -2,18 +2,39 @@
 # 0.1.0
 
 import sys
+from Tkinter import *
 
 # EDIT THIS IF YOU HAVE CHANGED YOUR DIRECTORY NAMES
 directory = 'work/abbrev_output.txt'
 
-name = raw_input("What is the mod's name?\n")
-abbrev = raw_input("What would you like its abbreviation to be?\n")
 
-fileout = open(directory, "w")
-fileout.write("|" + abbrev + " = {{#if:{{{name|}}}{{{code|}}}||_(}}{{#if:{{{name|}}}{{{link|}}}|" + name + "|" + abbrev + "}}{{#if:{{{name|}}}{{{code|}}}||)}}\n|" + name + " = {{#if:{{{name|}}}{{{code|}}}||_(}}{{#if:{{{name|}}}{{{link|}}}|" + name + "|" + abbrev + "}}{{#if:{{{name|}}}{{{code|}}}||)}}")
-fileout.close()
+def fn_run():
+    tk = Tk()
+    namevar = StringVar()
+    abrvvar = StringVar()
+    namelabel = Label(tk, text="Mod Name",  textvariable=namevar)
+    abrvlabel = Label(tk, text="Mod Abbreviation", textvariable=abrvvar)
+    quit = Button(tk, text="Close", command=tk.quit)
 
-sys.exit()
+    nameentry = Entry(tk)
+    abrventry = Entry(tk)
+
+    namelabel.pack()
+    nameentry.pack()
+    abrvlabel.pack()
+    abrventry.pack()
+    quit.pack()
+
+    tk.mainloop()
+    tk.quit()
+
+    fileout = open(directory, "w")
+    fileout.write("|" + abrvvar.get().capitalize + " = {{#if:{{{name|}}}{{{code|}}}||_(}}{{#if:{{{name|}}}{{{link|}}}|" + namevar.get() + "|" + abrvvar.get().capitalize + "}}{{#if:{{{name|}}}{{{code|}}}||)}}\n|" + namevar.get() + " = {{#if:{{{name|}}}{{{code|}}}||_(}}{{#if:{{{name|}}}{{{link|}}}|" + namevar.get() + "|" + abrvvar.get().capitalize + "}}{{#if:{{{name|}}}{{{code|}}}||)}}")
+    fileout.close()
+
+    sys.exit()
+
+fn_run()
 
 '''
 == Changelog ==
