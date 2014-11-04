37bc7f732d18effdda24efb35159f32597a53a3d
satanicsanta <elifosterwy@gmail.com>
2014-11-03 19:30:44 -0800

    more gui work

--- src/gui.py
+++ src/gui.py
@@ -2,6 +2,7 @@
 # Version 0.1.0
 
 from Tkinter import *
+from tkFileDialog import *
 import tkMessageBox
 from navitem import run
 import ScrolledText
@@ -11,23 +12,66 @@ import os
 
 directory = 'work/navitem_output.txt'
 
-def open_changelog():
+def fn_navchanges():
+    ch = Tk()
+    changelog = Message(ch, text="0.1.1\n* FIX: Changed output dir\n\n0.1.0\n* Initial")
+
+    changelog.pack()
+    ch.mainloop()
+
+def fn_abbrvchanges():
+    ch = Tk()
+    changelog = Message(ch, text="0.1.0\n* Initial")
+
+    changelog.pack()
+    ch.mainloop()
+
+def fn_nichanges():
+    ch = Tk()
+    changelog = Message(ch, text="0.1.6\n*FIX: Shoved everything in run() method\n\n0.1.5\n *FIX: Changed directories AGAIN\n\n0.1.4\n* FIX: Changed directories to work/navitem/input-output.txt, for when I add more programs\n\n0.1.3\n* FIX: Fixed tab spacing; it's no longer 4 spaces, and is actually indents\n\n0.1.2\n* FIX: No longer overwrites input.txt; instead it makes output.txt\n* FIX: It should work now,  though be weird, on non-UNIX systems (thanks Wolfman)\n* FIX: Program exits properly using sys.exit()\n\n0.1.1\n* FIX: Made the text that gets written actually correct\n* TWEAK: Changed modname raw_input text\n\n0.1.0\n* Initial")
+
+    changelog.pack()
+    ch.mainloop()
+
+def fn_guichanges():
     cl = Tk()
-    changelog = Message(cl, text="== Changelog ==\n=== navitem ===\n==== 0.1.6 ====\n*FIX: Shoved everything in run() method\n\n==== 0.1.5 ====\n *FIX: Changed directories AGAIN\n\n==== 0.1.4 ====\n* FIX: Changed directories to work/navitem/input-output.txt, for when I add more programs\n\n==== 0.1.3 ====\n* FIX: Fixed tab spacing; it's no longer 4 spaces, and is actually indents\n\n==== 0.1.2 ====\n* FIX: No longer overwrites input.txt; instead it makes output.txt\n* FIX: It should work now,  though be weird, on non-UNIX systems (thanks Wolfman)\n* FIX: Program exits properly using sys.exit()\n\n==== 0.1.1 ====\n* FIX: Made the text that gets written actually correct\n* TWEAK: Changed modname raw_input text\n\n==== 0.1.0 ====\n* Initial\n\n\n=== navbox ===\n==== 0.1.1 ====\n* FIX: Changed output dir\n\n==== 0.1.0 ====\n* Initial\n\n\n=== mod abbrev ===\n==== 0.1.0 ====\n* Initial")
+    changelog = Message(cl, text="0.1.0\n* Initial")
+
     changelog.pack()
     cl.mainloop()
 
-def navitem_():
+
+def fn_open_changelog():
+    cl = Tk()
+    navboxchanges = Button(cl, text="Navbox Program changelog", command=fn_navchanges)
+    abbrevchanges = Button(cl, text="Abbreviation Program changelog", command=fn_abbrvchanges)
+    nichanges = Button(cl, text="NI Program changelog", command=fn_nichanges)
+    guichanges = Button(cl, text="GUI Program changelog", command=fn_guichanges)
+
+    guichanges.pack()
+    nichanges.pack()
+    abbrevchanges.pack()
+    navboxchanges.pack()
+    cl.mainloop()
+
+def fn_openfile():
+    filedialog = askopenfilename()
+    filedialog.pack()
+
+def fn_navitem():
     nav = Tk()
     navlabel = Label(nav, text="Mod abbreviation")
     navlabel.pack()
-    naventry = Entry(nav, bd =5)
+    naventry = Entry(nav)
     naventry.pack()
 
-    textlabel = Label(nav, text="Input").pack()
-    ScrolledText.ScrolledText(nav, bd=5).pack(side=BOTTOM)
+    filebutt = Button(nav, text="Select Input File", command=fn_openfile)
+    filebutt.pack()
+
+
     nav.mainloop()
 
+'''
     tmp = tempfile.NamedTemporaryFile(delte=False)
     with open(directory) as finput:
         with open(tmp.name, 'w') as ftmp:
@@ -40,21 +84,22 @@ def navitem_():
         print "There was an issue using shutil! Trying to use os instead."
         os.rename(directory, directory)
     #run()
+'''
 
 
 
-def main():
+def fn_main():
     tk = Tk()
-    changelog_button = Button(tk, text="Changelog", command=open_changelog)
-    navitem_butt = Button(tk, text="navitem", command=navitem_)
-    quit = Button(tk, text="Quit", command=tk.quit)
+    changelog_button = Button(tk, text="Changelogs", command=fn_open_changelog)
+    navitem_butt = Button(tk, text="NI Template Program", command=fn_navitem)
+    quit = Button(tk, text="Quit Program", command=tk.quit)
 
-    quit.pack()
     navitem_butt.pack()
     changelog_button.pack()
+    quit.pack()
     tk.mainloop()
 
-main()
+fn_main()
 
 '''
 == Changelog ==
